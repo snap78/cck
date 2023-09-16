fbc39f8d4b97bcbcb878523c300a303b4d81dbf6
snap78 <145050116+snap78@users.noreply.github.com>
2023-09-16 16:16:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ window.onload = function() {
     if (button) {
         button.click();
         // Replace 'https://example.com' with your desired destination URL
-        window.location.href = "http://clickpavel2.social-networking.me/";
+        window.location.href = "adverpavl.social-networking.me";
     }
 };
